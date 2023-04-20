85dbd62ec77f8e84c52c537c6a04cf10d9d31535
Camila Brito <camilagbrito48@gmail.com>
2023-04-20 16:43:05 +0100

    Exercise Top Car Components

---
+++ src/routes/Catalog/index.tsx
@@ -0,0 +1,52 @@
+import Card from '../../components/Card';
+import Header from '../../components/Header';
+import SearchBar from '../../components/SearchBar';
+import './styles.css';
+
+export default function Catalog(){
+    return(
+        <>
+            <Header /> 
+            <main className="ct-catalog-main">
+                <section id="ct-catalog-section" className="ct-container">
+                    <div className="ct-search-bar-container">
+                        <SearchBar />
+                    </div>
+                    <div className="ct-catalog-cards-container">
+                        <div>
+                            <Card />
+                        </div>
+                        <div>
+                            <Card />
+                        </div>
+                        <div>
+                            <Card />
+                        </div>
+                        <div>
+                            <Card />
+                        </div>
+                        <div>
+                            <Card />
+                        </div>
+                        <div>
+                            <Card />
+                        </div>
+                        <div>
+                            <Card />
+                        </div>
+                        <div>
+                            <Card />
+                        </div>
+                        <div>
+                            <Card />
+                        </div>
+                        <div>
+                            <Card />
+                        </div>
+                    </div>
+                 
+                </section>
+            </main> 
+        </>
+    );
+}
